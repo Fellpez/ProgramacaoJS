b0c5b029890981fa4d190b10118a076c930f061a
Fellpez <contatofellipe17@gmail.com>
2026-08-17 14:48:02 -0300

    Revisao JS

---
+++ 03_revisao.js
@@ -0,0 +1,39 @@
+let aluguel = 1200.00
+let condominio = 859.9
+let internet = 119.99
+let total = (aluguel+condominio+internet)
+
+console.log (total)
+
+// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+
+let nome = "Fellipe"
+let sobrenome = "Pereira"
+let nome_completo = nome +" "+ sobrenome
+
+console.log (nome)
+console.log (sobrenome)
+console.log ("O nome junto fica " + nome_completo)
+
+// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+let salario = 2000
+
+console.log ("seu salário é de R$" + salario)
+
+let comissao = 200
+salario = salario + comissao
+
+console.log ("Salário com comissão R$"+ salario)
+
+let adicional_noturno = 130
+salario = salario + adicional_noturno
+
+console.log("Seu salário com adicional noturno é de R$" + salario)
+
+/*
+isasasasasasa
+ssasassasassasasas
+asasasasasasasasasasasa
+*/
